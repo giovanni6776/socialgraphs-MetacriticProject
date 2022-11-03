2df45925c91b3f3c8d7a0555219f11f7be48c36d
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-11-03 10:03:31 +0100

    get data

--- project_data_utils.py
+++ project_data_utils.py
@@ -1,3 +1,8 @@
 # Project Data Utils
-def getdata():
-    return None
+import pandas as pd
+
+## Return the final Pandas Data Frame to be used in the analysis 
+def get_data_game_line():
+    metacritic_df = pd.read_csv("data/metacritic_games_full.csv" ,index_col=0)
+    return metacritic_df
+
